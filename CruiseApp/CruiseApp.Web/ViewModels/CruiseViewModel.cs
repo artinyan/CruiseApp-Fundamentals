55e1fc65478a17530e16d22b04d6c87b99a3e053
artinyan <119803084+artinyan@users.noreply.github.com>
2026-02-07 14:29:47 +0000

    Added ViewModels folder. Added CruiseViewModel.

---
+++ CruiseApp/CruiseApp.Web/ViewModels/CruiseViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CruiseApp.Web.ViewModels
+{
+    /// <summary>
+    /// ViewModel for showing cruise.
+    /// </summary>
+    public class CruiseViewModel
+    {
+        public int Id { get; set; }
+        public string ShipName { get; set; } = null!;
+        public DateOnly FirstDay { get; set; }
+        public DateOnly LastDay { get; set; }
+        public int CruiseLength { get; set; }
+        public bool CanLike { get; set; }
+        public int LikesCount { get; set; }
+    }
+}
