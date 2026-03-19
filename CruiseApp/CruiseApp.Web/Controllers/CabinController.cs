dc7aa818e330400389d0dcae4a05bedf5552ab57
artinyan <119803084+artinyan@users.noreply.github.com>
2026-03-19 21:14:16 +0000

    Separation of Controllers.

---
+++ CruiseApp/CruiseApp.Web/Controllers/CabinController.cs
@@ -0,0 +1,93 @@
+﻿using CruiseApp.Data.Models.Enums;
+using CruiseApp.Services.Core.Interfaces;
+using CruiseApp.ViewModels.Cruise;
+using CruiseApp.ViewModels.Deck;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CruiseApp.Web.Controllers
+{
+    public class CabinController : Controller
+    {
+        private readonly ICruiseService cruiseService;
+
+        public CabinController(ICruiseService cruiseService)
+        {
+            this.cruiseService = cruiseService;
+        }
+
+        // ============================
+        // GET: /Cabin/Cabins
+        // ============================
+        public async Task<IActionResult> Cabins(int cruiseId)
+        {
+            var serviceModel = await cruiseService.GetCabinsAsync(cruiseId);
+
+            if (serviceModel == null)
+                return NotFound();
+
+            var model = new CabinsViewModel
+            {
+                CruiseId = serviceModel.CruiseId,
+                ShipName = serviceModel.ShipName,
+                StartPoint = serviceModel.StartPoint,
+                FirstDay = serviceModel.FirstDay,
+                LastDay = serviceModel.LastDay,
+                Nights = serviceModel.Nights,
+
+                Cabins = serviceModel.Cabins
+                    .Select(c => new CabinCardViewModel
+                    {
+                        ShipName = serviceModel.ShipName,
+                        CabinType = c.CabinType,
+                        CruiseId = serviceModel.CruiseId,
+                        Price = c.Price,
+
+                        Decks = c.Decks.Select(d => new DeckButtonViewModel
+                        {
+                            Id = d.Id,
+                            Name = d.Name
+                        })
+                    })
+                    .ToList()
+            };
+
+            return View(model);
+        }
+
+        // ============================
+        // GET: /Cabin/Deck
+        // ============================
+        public async Task<IActionResult> Deck(int cruiseId, int deckId, CabinType cabinType)
+        {
+            var serviceModel = await cruiseService.GetDeckCabinsAsync(cruiseId, deckId, cabinType);
+
+            if (serviceModel == null)
+                return NotFound();
+
+            var model = new DeckCabinsViewModel
+            {
+                CruiseId = serviceModel.CruiseId,
+                ShipName = serviceModel.ShipName,
+                DeckId = serviceModel.DeckId,
+                DeckName = serviceModel.DeckName,
+                DeckNumber = int.Parse(serviceModel.DeckName),
+                DeckImage = serviceModel.DeckImage,
+                CabinType = serviceModel.CabinType,
+                Cabins = serviceModel.Cabins
+                    .Select(c => new CabinButtonViewModel
+                    {
+                        Id = c.Id,
+                        Number = c.Number,
+                        Name = c.Name,
+                        CabinType = c.CabinType,
+                        PosX = c.PosX,
+                        PosY = c.PosY,
+                        IsAvailable = c.IsAvailable
+                    })
+                    .ToList()
+            };
+
+            return View(model);
+        }
+    }
+}
--- CruiseApp/CruiseApp.Web/Controllers/CruiseController.cs
+++ CruiseApp/CruiseApp.Web/Controllers/CruiseController.cs
@@ -7,7 +7,6 @@ using CruiseApp.ViewModels.Cruise;
 using CruiseApp.ViewModels.Deck;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
 
 namespace CruiseApp.Web.Controllers
@@ -22,8 +21,9 @@ namespace CruiseApp.Web.Controllers
         public CruiseController(
             ICruiseService cruiseService,
             IShipService shipService,
-            IPointService pointService,
-            ICruiseLikeService cruiseLikeService)
+            ICruiseLikeService cruiseLikeService,
+            IPointService pointService
+            )
         {
             this.cruiseService = cruiseService;
             this.shipService = shipService;
@@ -91,57 +91,6 @@ namespace CruiseApp.Web.Controllers
         //// ============================
         //// GET: /Cruise
         //// ============================
-        //[HttpGet]
-        //public async Task<IActionResult> Index([FromQuery] CruiseSearchViewModel model)
-        //{
-        //    // Ships dropdown
-        //    model.Ships = (await shipService.GetAllAsync())
-        //        .Select(s => new SelectListItem
-        //        {
-        //            Value = s.Id.ToString(),
-        //            Text = s.Name
-        //        })
-        //        .ToList();
-
-        //    // Start points dropdown (without "At Sea")
-        //    model.StartPoints = (await pointService.GetAllAsync())
-        //        .Where(p => !p.IsSea)
-        //        .Select(p => new SelectListItem
-        //        {
-        //            Value = p.Id.ToString(),
-        //            Text = p.Name
-        //        })
-        //        .ToList();
-
-        //    // Search cruises (map от Cruise -> CruiseListItemViewModel)
-        //    var cruises = await cruiseService.SearchCruisesAsync(
-        //        model.ShipId,
-        //        model.StartDate,
-        //        model.StartPointId);
-
-        //    model.Cruises = cruises.Select(c => new CruiseListItemViewModel
-        //    {
-        //        Id = c.Id,
-        //        ShipName = c.Ship.Name,
-        //        FirstDay = c.FirstDay,
-        //        LastDay = c.LastDay,
-        //        Nights = c.CruiseLength,
-
-        //        StartPoint = c.Route.Days
-        //            .Where(rd => rd.Date >= c.FirstDay && rd.Date <= c.LastDay)
-        //            .OrderBy(rd => rd.Date)
-        //            .Select(rd => rd.Point.Name)
-        //            .FirstOrDefault() ?? string.Empty,
-
-        //        Destinations = string.Join(" • ", c.Route.Days
-        //            .Where(rd => rd.Date >= c.FirstDay && rd.Date <= c.LastDay)
-        //            .OrderBy(rd => rd.Date)
-        //            .Select(rd => rd.Point.Name)),
-        //        EndPoint = string.Empty
-        //    }).ToList();
-
-        //    return View(model);
-        //}
 
         [HttpGet]
         public async Task<IActionResult> Details(int id)
@@ -191,8 +140,6 @@ namespace CruiseApp.Web.Controllers
             return View(model);
         }
 
-
-
         [Authorize(Roles = Roles.User)]
         [HttpPost]
         public async Task<IActionResult> Like(int id)
@@ -211,77 +158,73 @@ namespace CruiseApp.Web.Controllers
             return RedirectToAction(nameof(Details), new { id });
         }
 
-        public async Task<IActionResult> Cabins(int cruiseId)
-        {
-            var serviceModel = await cruiseService.GetCabinsAsync(cruiseId);
+        //public async Task<IActionResult> Cabins(int cruiseId)
+        //{
+        //    var serviceModel = await cruiseService.GetCabinsAsync(cruiseId);
 
-            if (serviceModel == null)
-                return NotFound();
+        //    if (serviceModel == null)
+        //        return NotFound();
 
-            var model = new CabinsViewModel
-            {
-                CruiseId = serviceModel.CruiseId,
-                ShipName = serviceModel.ShipName,
-                StartPoint = serviceModel.StartPoint,
-                FirstDay = serviceModel.FirstDay,
-                LastDay = serviceModel.LastDay,
-                Nights = serviceModel.Nights,
+        //    var model = new CabinsViewModel
+        //    {
+        //        CruiseId = serviceModel.CruiseId,
+        //        ShipName = serviceModel.ShipName,
+        //        StartPoint = serviceModel.StartPoint,
+        //        FirstDay = serviceModel.FirstDay,
+        //        LastDay = serviceModel.LastDay,
+        //        Nights = serviceModel.Nights,
 
-                Cabins = serviceModel.Cabins
-                    .Select(c => new CabinCardViewModel
-                    {
-                        ShipName = serviceModel.ShipName,
-                        CabinType = c.CabinType,
-                        CruiseId = serviceModel.CruiseId,
-                        Price = c.Price,
+        //        Cabins = serviceModel.Cabins
+        //            .Select(c => new CabinCardViewModel
+        //            {
+        //                ShipName = serviceModel.ShipName,
+        //                CabinType = c.CabinType,
+        //                CruiseId = serviceModel.CruiseId,
+        //                Price = c.Price,
 
-                        Decks = c.Decks.Select(d => new DeckButtonViewModel
-                        {
-                            Id = d.Id,
-                            Name = d.Name
-                        })
-                    })
-                    .ToList()
-            };
+        //                Decks = c.Decks.Select(d => new DeckButtonViewModel
+        //                {
+        //                    Id = d.Id,
+        //                    Name = d.Name
+        //                })
+        //            })
+        //            .ToList()
+        //    };
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
 
-      
+        //public async Task<IActionResult> Deck(int cruiseId, int deckId, CabinType cabinType)
+        //{
+        //    var serviceModel = await cruiseService
+        //        .GetDeckCabinsAsync(cruiseId, deckId, cabinType);
 
-        public async Task<IActionResult> Deck(int cruiseId, int deckId, CabinType cabinType)
-        {
-            var serviceModel = await cruiseService
-                .GetDeckCabinsAsync(cruiseId, deckId, cabinType);
+        //    if (serviceModel == null)
+        //        return NotFound();
 
-            if (serviceModel == null)
-                return NotFound();
-
-            var model = new DeckCabinsViewModel
-            {
-                CruiseId = serviceModel.CruiseId,
-                ShipName = serviceModel.ShipName,
-                DeckId = serviceModel.DeckId,
-                DeckName = serviceModel.DeckName,
-                DeckNumber = int.Parse(serviceModel.DeckName),
-                DeckImage = serviceModel.DeckImage,
-                CabinType = serviceModel.CabinType,
-                Cabins = serviceModel.Cabins
-                    .Select(c => new CabinButtonViewModel
-                    {
-                        Id = c.Id,
-                        Number = c.Number,
-                        Name = c.Name,
-                        CabinType = c.CabinType,
-                        PosX = c.PosX,
-                        PosY = c.PosY,
-                        IsAvailable = c.IsAvailable
-                    })
-                    .ToList()
-            };
-            return View(model);
-        }
-
-
+        //    var model = new DeckCabinsViewModel
+        //    {
+        //        CruiseId = serviceModel.CruiseId,
+        //        ShipName = serviceModel.ShipName,
+        //        DeckId = serviceModel.DeckId,
+        //        DeckName = serviceModel.DeckName,
+        //        DeckNumber = int.Parse(serviceModel.DeckName),
+        //        DeckImage = serviceModel.DeckImage,
+        //        CabinType = serviceModel.CabinType,
+        //        Cabins = serviceModel.Cabins
+        //            .Select(c => new CabinButtonViewModel
+        //            {
+        //                Id = c.Id,
+        //                Number = c.Number,
+        //                Name = c.Name,
+        //                CabinType = c.CabinType,
+        //                PosX = c.PosX,
+        //                PosY = c.PosY,
+        //                IsAvailable = c.IsAvailable
+        //            })
+        //            .ToList()
+        //    };
+        //    return View(model);
+        //}
     }
 }
